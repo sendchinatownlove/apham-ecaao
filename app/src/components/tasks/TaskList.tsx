4460d09c1d16f80a2a5608fb06addb0f88ff0f8c
emilyw26 <emilywu1456@gmail.com>
2023-04-13 20:48:31 -0400

    Add transparent container

--- app/src/components/tasks/TaskList.tsx
+++ app/src/components/tasks/TaskList.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 export const TaskListContainer = styled.div`
-  height: 100%;
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 25px;
+  min-height: 480px;
+  min-width: 300px;
+  text-align: center;
+  margin-top: 20px;
 `
 
 export default function TaskList() {
