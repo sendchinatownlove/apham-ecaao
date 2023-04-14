317728f9ce95587dfbfc909d71900419ac3c60e6
emilyw26 <emilywu1456@gmail.com>
2023-04-13 20:33:09 -0400

    Set background

--- app/src/components/tasks/TaskList.tsx
+++ app/src/components/tasks/TaskList.tsx
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
+export const TaskListContainer = styled.div`
+  height: 100%;
+`
+
 export default function TaskList() {
-  return <h2>TaskList!</h2>;
+  return (
+      <TaskListContainer>
+        <h2>TaskList!</h2>
+      </TaskListContainer>
+  );
 }
