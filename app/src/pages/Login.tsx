5c6f2020a7b92a12ca7cacfb28813df66ad20d67
Amanda Liu <aliu@patreon.com>
2023-04-16 11:40:01 -0400

    [SCL-23] Login page more styling and add logo asset (#7)

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,64 +1,147 @@
-import React from "react";
-
 import styled from "styled-components";
 
-const BrandText = styled.div`
-  font-family: "Open Sans";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 19px;
-  text-transform: uppercase;
+import Logo from "../assets/logo.svg";
+import { BrandText } from "../styled-components";
 
-  color: #a8192e;
+const PageContainer = styled.div`
+  background: #ffffff;
+  width: 367px;
+  height: 611px;
+  padding: 14px;
 `;
 
-const BrandSubtext = styled.div`
-  font-family: "Open Sans";
-  font-style: italic;
-  font-weight: 300;
-  font-size: 10px;
-  line-height: 14px;
+const HeaderText = styled(BrandText)<{ fontSize?: number }>`
   letter-spacing: 0.15em;
-  text-transform: uppercase;
+  font-weight: 700;
+  padding-bottom: 4px;
 
-  color: #a8192e;
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}px`};
 `;
 
-const CtaText = styled.div`
-  font-family: "Open Sans";
-  font-style: normal;
-  font-size: 14px;
-  line-height: 19px;
+const SubheaderText = styled(BrandText)`
+  font-style: italic;
+  letter-spacing: 0.15em;
+  font-size: 10px;
+  position: relative;
+  max-width: 600px;
   text-align: center;
+  margin: auto;
 
-  color: #000000;
+  &:before {
+    content: "";
+    width: 54px;
+    height: 0.5px;
+    background: #a8192e;
+    left: 16px;
+    top: 50%;
+    position: absolute;
+  }
+
+  &:after {
+    content: "";
+    width: 54px;
+    height: 0.5px;
+    background: #a8192e;
+    right: 16px;
+    top: 50%;
+    position: absolute;
+  }
 `;
 
-const EmailInput = styled.div`
-  display: flex;
+const CtaText = styled(BrandText)`
+  color: #000000;
+  padding-top: 24px;
+`;
+
+const InputWrapper = styled.div`
+  padding-top: 32px;
+  width: 331px;
+  margin: auto;
+`;
+
+const InputLabel = styled(BrandText)`
+  color: #000000;
+  text-align: left;
+`;
+
+const EmailInput = styled.input`
+  width: 331px;
+  height: 50px;
+  box-sizing: border-box;
+  background: #ffffff;
+  border: 1px solid #dadada;
+  border-radius: 5px;
+  margin-top: 12px;
+`;
+
+const ButtonWrapper = styled.button`
+  background: #8b8b8b;
+  border-radius: 50px;
+  width: 267px;
+  height: 45px;
+  margin-top: 74px;
+  margin-bottom: 80px;
+`;
+
+const ButtonText = styled(BrandText)`
+  letter-spacing: 0.15em;
+  color: #ffffff;
+  font-weight: 700;
+`;
+
+const LogoWrapper = styled.div`
+  display: block;
+  position: relative;
+  margin: auto;
+
+  &:before {
+    content: "";
+    width: 164px;
+    height: 0.5px;
+    background: #a8192e;
+    left: 14px;
+    top: 42%;
+    position: absolute;
+  }
+
+  &:after {
+    content: "";
+    width: 164px;
+    height: 0.5px;
+    background: #a8192e;
+    right: 14px;
+    top: 42%;
+    position: absolute;
+  }
 `;
 
 export default function LoginPage() {
   return (
-    <div>
-      <BrandText>100* WAYS TO SEND CHINATOWN LOVE</BrandText>
-      <BrandSubtext>
-        {" "}
-        --------- EVERYTHING CHINATOWN ALL AT ONCE ---------
-      </BrandSubtext>
-      <CtaText>Hey explorer!</CtaText>
+    <PageContainer>
+      <HeaderText>100* WAYS TO SEND CHINATOWN LOVE</HeaderText>
+      <SubheaderText>EVERYTHING CHINATOWN ALL AT ONCE</SubheaderText>
       <CtaText>
+        <b>Hey explorer!</b>
+        <br />
         Enter your email below to start exploring the Chinatowns across the
         boroughs and log your completed activities for chances to win prizes.
       </CtaText>
-      <EmailInput>
-        <CtaText>Email address</CtaText>
-        <input type="text" id="email" name="email" />
-      </EmailInput>
-      <button>Enter</button>
-      <BrandText>READ ABOUT THIS EVENT</BrandText>
-      <BrandText>@SENDCHINATOWNLOVE</BrandText>
-    </div>
+      <InputWrapper>
+        <InputLabel>Email address</InputLabel>
+        <EmailInput type="text" id="email" name="email" />
+      </InputWrapper>
+      <ButtonWrapper>
+        <ButtonText>ENTER</ButtonText>
+      </ButtonWrapper>
+      <LogoWrapper>
+        <img alt="logo" src={Logo} />
+      </LogoWrapper>
+      <div style={{ paddingTop: "12px" }}>
+        <HeaderText fontSize={12}>READ ABOUT THIS EVENT</HeaderText>
+      </div>
+      <div style={{ paddingTop: "12px" }}>
+        <HeaderText fontSize={12}>@SENDCHINATOWNLOVE</HeaderText>
+      </div>
+    </PageContainer>
   );
 }
