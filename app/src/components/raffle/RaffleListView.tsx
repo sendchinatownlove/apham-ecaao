a81415b4d22627ab6dafc42848dcb850233c870b
Cina10 <li.energy4@gmail.com>
2023-04-24 11:23:20 -0400

    Add Confirmation modal

--- app/src/components/raffle/RaffleListView.tsx
+++ app/src/components/raffle/RaffleListView.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import styled from "styled-components";
 import TaskListBackButton from "../tasks/TaskListBackButton";
 import RaffleHeader from "./RaffleHeader";
 import RaffleList, { RafflePrizeData } from "./RaffleList";
+import EntryConfirmationModal from "./EntryConfirmationModal";
 
 const RaffleViewContainer = styled.div`
   background-color: rgba(255,255,255,0.3);
@@ -21,12 +23,16 @@ export default function RaffleListView(props: RaffleViewProps) {
     const { prizeData } = props;
 
     let navigate = useNavigate();
+    const [isPopupActive, setIsPopupActive] = useState(false);
 
     return (
+        <>
         <RaffleViewContainer>
             <TaskListBackButton onClick={() => {navigate('/', { replace: true })}}/>
             <RaffleHeader enteredTickets={11} availableTickets={6}></RaffleHeader>
             <RaffleList prizeData={prizeData}></RaffleList>
         </RaffleViewContainer>
+        <EntryConfirmationModal isActive={isPopupActive} setIsActive={setIsPopupActive}/>
+        </>
     );
 }
