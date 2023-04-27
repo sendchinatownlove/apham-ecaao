b5667ccc3fc877cd98ec53141bb8cd1ddb971a51
Jacob Barnett <2818246+jacoblb64@users.noreply.github.com>
2023-04-26 22:11:01 -0400

    feat: working and deployed

--- functions/gcp-signed-url/src/index.ts
+++ functions/gcp-signed-url/src/index.ts
@@ -1,15 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-require("dotenv").config();
-
-// For local testing (maybe not working?)
-// const creds = require("../credentials.json");
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(creds),
-// });
 
 admin.initializeApp();
+require("dotenv").config();
 
 const cors = require("cors")({
     origin: true,
@@ -31,15 +24,15 @@ type fileInfoDTO = {
 
 exports.generateV4UploadSignedUrl = functions.https.onRequest(async (req, res) => {
     cors(req, res, async () => {
-        // res.set("Access-Control-Allow-Origin", "*");
+        res.set("Access-Control-Allow-Origin", "*");
 
         switch (req.method) {
             case "POST":
                 console.log(JSON.stringify(req.body));
-                // if (!req.body || !req.body["filename"] || !req.body["filetype"]) {
-                //     res.status(500).send({ message: "missing fields" });
-                //     return;
-                // }
+                if (!req.body || !req.body["filename"] || !req.body["filetype"]) {
+                    res.status(500).send({ message: "missing fields" });
+                    break;
+                }
 
                 try {
                     const url = await generateV4UploadSignedUrl(req.body);
@@ -47,15 +40,27 @@ exports.generateV4UploadSignedUrl = functions.https.onRequest(async (req, res) =
                     res.status(200).send({ url: url });
                 } catch (error) {
                     functions.logger.error("Error when generating upload url:", error);
-                    // res.status(500).send({ url: "", message: "server-side error when creating upload url" });
+                    res.status(500).send({ url: "", message: "server-side error when creating upload url" });
                 }
-                return;
+                break;
             default:
-                // res.status(404).send({ message: "unsupported" });
+                res.status(404).send({ message: "unsupported" });
         }
     });
 });
 
+/**
+ * The actual call to the Google Storage SDK to generate a signed upload URL
+ *
+ * Can test locally by calling the function like:
+ * generateV4UploadSignedUrl({filename: "testname.jpg", filetype: "image/jpg"});
+ *
+ * and then uploading like:
+ * curl -X PUT -H 'Content-Type: ${fileInfo.filetype}' ` + `--upload-file my-file '${url}'
+ *
+ * @param fileInfo The file info tuple
+ * @returns the Signed URL for upload
+ */
 async function generateV4UploadSignedUrl(fileInfo: fileInfoDTO) {
     const options = {
         version: "v4",
@@ -66,14 +71,5 @@ async function generateV4UploadSignedUrl(fileInfo: fileInfoDTO) {
 
     const [url] = await storage.bucket(bucketName).file(fileInfo.filename).getSignedUrl(options);
 
-    // Uncomment for local testing
-    // console.log("Generated PUT signed URL:");
-    // console.log(url);
-    // console.log("You can use this URL with any user agent, for example:");
-    // console.log(`curl -X PUT -H 'Content-Type: ${fileInfo.filetype}' ` + `--upload-file my-file '${url}'`);
-
     return url;
 }
-
-// Uncomment the following line to test locally
-// generateV4UploadSignedUrl({filename: "testname.jpg", filetype: "image/jpg"});
